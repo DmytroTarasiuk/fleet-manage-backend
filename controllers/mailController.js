9230878ba199de86d13d54b5c56f95b7e7c5c97c
DmytroTarasiuk <dmytro.tarasiuk1899@gmail.com>
2024-01-29 13:50:52 +0100

    add email template

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -1,11 +1,37 @@
 const nodemailer = require("nodemailer");
 const dotenv = require("dotenv");
+const ejs = require("ejs");
+const fs = require("fs");
 
 dotenv.config();
 
 const sendEmail = async (req, res) => {
   try {
-    const { to, subject, text } = req.body;
+    const { to, subject } = req.body;
+
+    const data = {
+      obrotBolt: 797.61,
+      gotowkaBolt: 797.61,
+      bonusBolt: 797.61,
+      obrotUber: 500.0,
+      gotowkaUber: 500.0,
+      bonusUber: 500.0,
+      obrotFn: 0,
+      gotowkaFn: 0,
+      bonusFn: 0,
+      vat: 278,
+      prowizja: 40,
+      wynajem: 0,
+      regulacja: 0,
+      wyplata: 2000,
+    };
+
+    const htmlTemplate = fs.readFileSync(
+      "./templates/emailTemplate.ejs",
+      "utf-8"
+    );
+
+    const renderedHtml = ejs.render(htmlTemplate, data);
 
     const transporter = nodemailer.createTransport({
       service: "gmail",
@@ -19,7 +45,7 @@ const sendEmail = async (req, res) => {
       from: "dmytro.tarasiuk1899@gmail.com",
       to,
       subject,
-      text,
+      html: renderedHtml,
     };
 
     const info = await transporter.sendMail(mailOptions);
