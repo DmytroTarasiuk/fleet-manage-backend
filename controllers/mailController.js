38eb5fecff014f394a3ac4c651a27e0bb1986c6a
DmytroTarasiuk <dmytro.tarasiuk1899@gmail.com>
2024-02-02 22:35:50 +0100

    add data for sending an email

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -7,23 +7,24 @@ dotenv.config();
 
 const sendEmail = async (req, res) => {
   try {
-    const { to, subject } = req.body;
+    const { to, subject, data } = req.body;
 
-    const data = {
-      obrotBolt: 797.61,
-      gotowkaBolt: 797.61,
-      bonusBolt: 797.61,
-      obrotUber: 500.0,
-      gotowkaUber: 500.0,
-      bonusUber: 500.0,
-      obrotFn: 0,
-      gotowkaFn: 0,
-      bonusFn: 0,
-      vat: 278,
-      prowizja: 40,
-      wynajem: 0,
-      regulacja: 0,
-      wyplata: 2000,
+    const dataToSend = {
+      obrotBolt: data.profitBolt || 0,
+      gotowkaBolt: data.cashBolt || 0,
+      bonusBolt: data.bonusBolt || 0,
+      obrotUber: data.profitUber || 0,
+      gotowkaUber: data.cashUber || 0,
+      bonusUber: data.bonusUber || 0,
+      obrotFn: data.profitFn || 0,
+      gotowkaFn: data.cashFn || 0,
+      bonusFn: data.bonusFn || 0,
+      vat: data.vat || 0,
+      vatBonus: data.vatBonus || 0,
+      prowizja: data.comission || 0,
+      wynajem: data.rent || 0,
+      regulacja: data.adjustments || 0,
+      wyplata: data.salary || 0,
     };
 
     const htmlTemplate = fs.readFileSync(
@@ -31,18 +32,18 @@ const sendEmail = async (req, res) => {
       "utf-8"
     );
 
-    const renderedHtml = ejs.render(htmlTemplate, data);
+    const renderedHtml = ejs.render(htmlTemplate, dataToSend);
 
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        user: "dmytro.tarasiuk1899@gmail.com",
+        user: process.env.EMAIL_ADDRESS,
         pass: process.env.EMAIL_PASS,
       },
     });
 
     const mailOptions = {
-      from: "dmytro.tarasiuk1899@gmail.com",
+      from: process.env.EMAIL_ADDRESS,
       to,
       subject,
       html: renderedHtml,
